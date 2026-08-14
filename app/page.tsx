ade1f749fc741c179b871c9968a1015be186f4be
muthukumar-sde <muthukumar@starberry.tv>
2026-08-14 15:14:13 +0530

    chore: remove unused raw HEIC images, dead code components, and integrate BirthdayMessage and Footer

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import MusicPlayer from './components/MusicPlayer';
 import Timeline from './components/Timeline';
 import PhotoGallery from './components/PhotoGallery';
 import NameStory from './components/NameStory';
+import BirthdayMessage from './components/BirthdayMessage';
 import BirthdayCelebration from './components/BirthdayCelebration';
+import Footer from './components/Footer';
 import CustomCursor from './components/CustomCursor';
 import ScrollProgress from './components/ScrollProgress';
 import BackgroundCrackers from './components/BackgroundCrackers';
@@ -22,7 +24,9 @@ export default function Home() {
       <Timeline />
       <PhotoGallery />
       <NameStory />
+      <BirthdayMessage />
       <BirthdayCelebration />
+      <Footer />
     </main>
   );
 }
